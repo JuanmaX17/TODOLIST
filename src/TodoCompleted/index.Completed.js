4053f4c53374b94123c6750fda6fcff1a8480900
Juan Manuel <juanmaue036@gmail.comm>
2022-04-23 19:57:52 -0500

    make id

--- src/TodoCompleted/index.Completed.js
+++ src/TodoCompleted/index.Completed.js
@@ -6,9 +6,10 @@ import {TodoContext} from "../TodoContext/index.Context"
 export function TodoCompleted(){
     
     const {completedTodo,totalTodo} = React.useContext(TodoContext);
-
+    let clasePadding =  (totalTodo > 4) && "padding"
+    console.log("ass" + clasePadding)
     return (
-       <div className="todo__title">
+       <div className={`todo__title ${clasePadding}`}>
             
         <h1 className="todo__text__title "> Tareas completadas</h1>  
         <p className="todo__text__title">{completedTodo} de {totalTodo}</p>      
