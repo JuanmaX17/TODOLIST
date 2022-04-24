6b7513813e4c6e27c07794ae8db7196b506cb913
Juan Manuel <juanmaue036@gmail.comm>
2022-04-24 10:06:48 -0500

    make it better

--- src/TodoCompleted/index.Completed.js
+++ src/TodoCompleted/index.Completed.js
@@ -6,10 +6,10 @@ import {TodoContext} from "../TodoContext/index.Context"
 export function TodoCompleted(){
     
     const {completedTodo,totalTodo,searchedTodo} = React.useContext(TodoContext);
-    let clasePadding =  (searchedTodo > 4) && "padding"
-    console.log("ass" + clasePadding)
+    
+ 
     return (
-       <div className={`todo__title ${clasePadding}`}>
+       <div className={`todo__title`}>
             
         <h1 className="todo__text__title "> Tareas completadas</h1>  
         <p className="todo__text__title">{completedTodo} de {totalTodo}</p>      
